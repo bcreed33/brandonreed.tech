9868c90a4c8dd750db537100746c3b2fc2f532d1
bcreed33 <bcreedesign@gmail.com>
2020-02-15 02:35:30 -0500

    working on more js features

--- js/script.js
+++ js/script.js
@@ -33,21 +33,13 @@ document.querySelector('.cd-member-bio-close').addEventListener('click', functio
 
 ////All my portfolio peices info. Hope soon to transfer this info into a headless cms platform
 const portP =[{
-    portfolio_id : "1",
-    portfolio_title : "Inspira",
-    portfolio_imageUrl : "img/inspiraCapture.jpg",
-    portfolio_link : "http://www.inspirafs.com",
-    portfolio_copy : "On this project, I was the lead for all design aspects of the website. Everything from designing the website on all screen sizes, creating supporting icons, choosing imagery, and selecting the colors. I added sprites to the website to cut down on http requests to help keep this website lean and fast. We also added structure data to help with the Google ranking and SEO.",
-    portfolio_talent : [{
-        portfolio_talent1 : "talent1",
-        portfolio_talent2 : "talent2",
-        portfolio_talent3 : "talent3"
-                        }]
-},{
-    portfolio_id : "2",
-    portfolio_title : "Tapolci Foundation",
+    portfolio_id : "0",
+    portfolio_title : "4moms",
     portfolio_imageUrl : "img/tpfCapture.jpg",
-    portfolio_link : "http://www.tapolcifoundation.org/",
+    portfolio_hasGithub: true,
+    portfolio_githubUrl: "",
+    portfolio_hasurl : false,
+    portfolio_url : "http://www.tapolcifoundation.org/",
     portfolio_copy : "For the Tapolci Foundation website, I worked with a team and was responsible for all the front-end related tasks. I made sure the website was fully responsive and took care of other things like typography, creating custom graphics, and taking the photographs that were shown on the website.",
     portfolio_talent : [{
         portfolio_talent1 : "talent4",
@@ -55,10 +47,13 @@ const portP =[{
         portfolio_talent3 : "talent6"
                         }]
 },{
-    portfolio_id : "3",
+    portfolio_id : "1",
     portfolio_title : "The Batters Box",
     portfolio_imageUrl : "img/battersboxScreenGrab.JPG",
-    portfolio_link : "http://thebattersbox.letsreedesign.com/",
+    portfolio_hasGithub: true,
+    portfolio_githubUrl: "",
+    portfolio_hasurl : true,
+    portfolio_url : "http://thebattersbox.letsreedesign.com/",
     portfolio_copy : "The Batters Box is a fictitious restaurant that I came up with. I first developed the website statically then converted it over to a WordPress platform. This website is my playground for WordPress development, whenever I learn something new about WordPress this will be the place where I try it out. Right now this website use plugins such as ACF, contact form 7, icegram, and The Events Calendar.",
     portfolio_talent : [{
         portfolio_talent1 : "talent7",
@@ -66,19 +61,37 @@ const portP =[{
         portfolio_talent3 : "talent8"
                         }]
 },{
-    portfolio_id : "4",
+    portfolio_id : "2",
     portfolio_title : "Going Deeper",
     portfolio_imageUrl : "img/goingDeeperScreenGrab.jpg",
-    portfolio_link : "https://goingdeeperconference.letsreedesign.com/",
+    portfolio_hasGithub: true,
+    portfolio_githubUrl: "",
+    portfolio_hasurl : false,
+    portfolio_url : "https://goingdeeperconference.letsreedesign.com/",
     portfolio_copy : "This project needed a quick turn-a-around time of two weeks, due to the event quickly approaching. So with a short timeline, I used webflow to allow me to produce a website demo within a day. After I got the go-ahead form the demo, I worked on completing the static website. Phase two of this project was converting the static website into a WordPress website. With the use of Advanced Custom Fields, I gave my client complete control of all their content, on the website.",
     portfolio_talent : [{
         portfolio_talent1 : "talent10",
         portfolio_talent2 : "talent12",
         portfolio_talent3 : "talent13"
                         }]
-    }
+},{
+    portfolio_id : "3",
+    portfolio_title : "Javascript Apps",
+    portfolio_imageUrl : "img/battersboxScreenGrab.JPG",
+    portfolio_hasGithub: true,
+    portfolio_githubUrl: "",
+    portfolio_hasurl : true,
+    portfolio_url : "http://thebattersbox.letsreedesign.com/",
+    portfolio_copy : "The Batters Box is a fictitious restaurant that I came up with. I first developed the website statically then converted it over to a WordPress platform. This website is my playground for WordPress development, whenever I learn something new about WordPress this will be the place where I try it out. Right now this website use plugins such as ACF, contact form 7, icegram, and The Events Calendar.",
+    portfolio_talent : [{
+        portfolio_talent1 : "talent7",
+        portfolio_talent2 : "talent9",
+        portfolio_talent3 : "talent8"
+                        }]
+}
 ];
 
+console.log(portP[1].portfolio_hasurl);
 
 //////js that is displaying my portfolio cards
 const porfolioCards = portP.map(x =>
@@ -106,14 +119,18 @@ const talentListMapped = talentList.map(x =>
      ${x.portfolio_talent3} <br>
     `);
 
+
 return `<div class="cd-member-bio member-${x.portfolio_id}">
     <div class="preview">
         <img src="${x.portfolio_imageUrl}" alt="Inspira website screen capture">
     </div>
     <div class="cd-bio-content">
         <h1>${x.portfolio_title}</h1>
-        <a href="${x.portfolio_link}" target="_blank" class="button text-center">
+        <a href="${x.portfolio_url}" target="_blank" class="button urlBtn text-center">
             Launch Site
+        </a>
+        <a href="${x.portfolio_githubUrl}" target="_blank" class="button githubBtn text-center">
+        View Code
         </a>
         <br>
         <h2>
@@ -129,12 +146,24 @@ return `<div class="cd-member-bio member-${x.portfolio_id}">
             ${x.portfolio_copy}
         </p>
     </div>
-</div>` });
+</div>` 
+});
 
 document.querySelector('#portfolioBios').innerHTML= porfolioBios;
 
 
-
+portP.map( function (x){
+    const portfolioNumber = x.portfolio_id;
+    const p = portfolioNumber.toString();
+    if(x.portfolio_hasurl == false){
+        const linkBtns = document.getElementsByClassName("urlBtn")[p];
+        linkBtns.classList.add("nolink");
+     };
+     if(x.portfolio_hasGithub == false){
+        const linkBtns = document.getElementsByClassName("githubBtn")[p];
+        linkBtns.classList.add("nolink");
+     };
+});
 
 //////js for the stick nav on scroll
 //Intersection Observer
