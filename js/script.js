be1c9caf6d869cf3f03c1cdcb232ff63bd3ba9b3
bcreed33 <bcreedesign@gmail.com>
2020-07-13 19:05:16 -0400

    breaking the js into modules

--- js/script.js
+++ js/script.js
@@ -6,212 +6,19 @@ navTrigger.addEventListener('click', function(e) {
 }, false);
 
 
-
-//////js for my portfolio slideout boxes
-//This is going to open my portfolio peices based off the data attribute I sent for each peice. 
-function myScript(x){
-    const selected_portfolio= x.getAttribute('data-type');
-    console.log(selected_portfolio);
-    
-    document.querySelector('body').className="freeze";
-    document.querySelector('.portfolio-overlay').className +=" portfolio-overlay-visable";
-    document.querySelector(`.portfolio-piece.${selected_portfolio}`).className += " slide-in";
-    document.querySelector('.portfolio-close').className += " is-visible";
-}
-//This is going to close the portfolio peices 
-document.querySelector('.portfolio-overlay, .portfolio-close is-visible').addEventListener('click', closePortfolioSelection);
-document.querySelector('.portfolio-close').addEventListener('click', closePortfolioSelection);
-function closePortfolioSelection(){
-    document.querySelector('body').classList.remove("freeze");
-    document.querySelector('.portfolio-overlay').classList.remove("portfolio-overlay-visable");
-    document.querySelector('.portfolio-piece.slide-in').classList.remove("slide-in");
-    document.querySelector('.portfolio-close').classList.remove("is-visible");
-};
-//this is closing the light box
-document.querySelector('.lightBox-close').addEventListener('click', closeLightBox);
-document.querySelector('.lightBoxWrapper').addEventListener('click', closeLightBox);
-function closeLightBox(){
-    document.querySelector('.lightBox-close').classList.remove("is-visible");
-    document.querySelector('.lightBoxWrapper').classList.remove("showLightBox");
-};
-
-
-document.addEventListener('keydown', logKey);
-function logKey(e) {
-    //console.log(e);
-    if (e.key == "Escape") {
-        const lightBoxIsOpen = document.querySelector('div.lightBoxWrapper').classList.contains("showLightBox");
-        const onlyPortfolioIsOpen = document.querySelector('.portfolio-overlay').classList.contains("portfolio-overlay-visable");
-        if (lightBoxIsOpen === true) {
-            closeLightBox();
-        }if(onlyPortfolioIsOpen === true && lightBoxIsOpen === false){
-            closePortfolioSelection();
-        }else{};
-    };
-};
-
-////All my portfolio peices info. Hope soon to transfer this info into a headless cms platform
-const portP =[{
-    portfolio_id : "0",
-    portfolio_title : "4moms",
-    portfolio_imageUrl : "img/4moms/4moms-preview.png",
-    portfolio_githubUrl: "",
-    portfolio_url : "https://goingdeeperconference.letsreedesign.com/",
-    portfolio_copy : "The current 4moms website is on the Shopify Plus platform with our support center on zendesk. That fact that the vast majority of our domestic web presence is now all on one URL/ framework is extremely beneficial for us. It's useful not only for me as the developer but for analytical tracking and user experiences. ",
-    portfolio_talent : ["Shopify", "Zendesk","Amazon s3"],
-    portfolio_screenShots : {
-        screenShot: "img/4moms/4moms-homepage.png",
-        screenShot2: "img/4moms/4moms-pdp.png",
-        screenShot3: "img/4moms/4moms-shopall.png",
-        screenShot4: "img/4moms/4moms-cares.png"
-
-    }
-},{
-    portfolio_id : "1",
-    portfolio_title : "4moms (Pre-2020)",
-    portfolio_imageUrl : "img/old4moms/old4moms-preview.png",
-    portfolio_githubUrl: "",
-    portfolio_url : "",
-    portfolio_copy : "The Pre-2020 4moms website / domestic web presence was composed of two different websites. One was our brochure-style website that was built on a ruby on rails platform. The other was our shop website that was built on a BigCommers platform. We use a handful of third party plugins and homegrown software that was also a vital part of this website tech stack.",
-    portfolio_talent : ["BigCommerce", "Ruby on Rails"],
-    portfolio_screenShots : {
-        screenShot: "img/old4moms/old4moms-mobile.jpg",
-        screenShot2: "img/old4moms/old4moms-homepage.png",
-        screenShot3: "img/old4moms/old4moms-sdp.png",
-        screenShot4: "img/old4moms/old4moms-pdp.png",
-        screenShot5: "img/old4moms/old4moms-support.png",
-        screenShot6: "img/old4moms/old4moms-international.png"
-
-
-    }
-},{
-    portfolio_id : "2",
-    portfolio_title : "Javascript Apps",
-    portfolio_imageUrl : "img/javascriptLandingpage.png",
-    portfolio_githubUrl: "https://github.com/bcreed33/javascriptAppCollection",
-    portfolio_url : "https://apps.brandonreed.tech/",
-    portfolio_copy : "Here's a gallery of javascript app that I have created. I have created some of them from scratch, from a tutorial or a combination of both. I'm always working on some new apps. So checkout out my GitHub to see what I'm working on.",
-    portfolio_talent : ["Vanilla JavaScript"],
-    portfolio_screenShots : {
-        screenShot: "img/javascriptLandingpage2.png"
-
-    }
-},{
-    portfolio_id : "3",
-    portfolio_title : "The Batters Box",
-    portfolio_imageUrl : "img/battersboxScreenGrab.JPG",
-    portfolio_githubUrl: "https://goingdeeperconference.letsreedesign.com/",
-    portfolio_url : "https://battersbox.brandonreed.tech/",
-    portfolio_copy : "The Batters Box is a fictitious restaurant that I created. Originally this website was a WordPress website. Its currently a statice website that I use as a playground for anything new that I might learn. ",
-    portfolio_talent : ["JavaScript"],
-    portfolio_screenShots : {
-        screenShot: "img/battersBox/batterboxHome.jpg",
-        screenShot2: "img/battersBox/batterboxMenu.png"
-
-    }
-}];
-
-console.log(portP[1].portfolio_hasurl);
-
-
 //////js that is displaying my portfolio cards
-const porfolioCards = portP.map(x =>
-`<li data-type="portfolio-${x.portfolio_id}" onclick="myScript(this);">
-    <a data-type="portfolio-${x.portfolio_id}">
-        <div class="portPeicePreview" style="background-image: url(../${x.portfolio_imageUrl});">
-            <div class="portPeiceTitle">
-                <h3>${x.portfolio_title}</h3>
-                <button data-type="member-${x.portfolio_id}" onclick="myScript(this);">More Info</button>
+const porfolioCards = portfolioPieces.map(x =>
+    `<li data-type="portfolio-${x.portfolio_id}" onclick="myScript(this);">
+        <a data-type="portfolio-${x.portfolio_id}">
+            <div class="portPeicePreview" style="background-image: url(../${x.portfolio_imageUrl});">
+                <div class="portPeiceTitle">
+                    <h3>${x.portfolio_title}</h3>
+                    <button data-type="member-${x.portfolio_id}" onclick="myScript(this);">More Info</button>
+                </div>
             </div>
-        </div>
-    </a>
-</li>`).join('');
-document.querySelector('#portPPP').innerHTML= porfolioCards;
-
-
-function lightBox(lightboxImage){
-    console.log("this is the lightbox function");
-    console.log(lightboxImage);
-    let imgURL = lightboxImage.getAttribute('data-src');
-    const lightBoxWrapper = document.querySelector('.lightBoxWrapper');
-    const theLightBox = document.querySelector('.lightBox');
-    theLightBox.innerHTML= `
-    <img src="${imgURL}">
-    `;
-    lightBoxWrapper.classList.add("showLightBox");
-    document.querySelector('.lightBox-close').classList.add("is-visible");
-
-
-
-};
-
-
-
-
-//////js that is filling out the info for the portfolio slide-out boxes
-const porfolioBios = portP.map(function (x){
-const talentList = x.portfolio_talent;
-//Mapping through the talent list array
-const talentListMapped = talentList.map(talent => `<p>${talent}</p>`).join('');
-// Storeing the object values inside the portfolio_screenShots in this const
-const screenShotList = Object.values(x.portfolio_screenShots);
-const screenShotImages = screenShotList.map( imgURL => `
-<div onclick="lightBox(this);" data-src="${imgURL}" class="screenshotOverlayContents">
-    <div class="screenshotOverlay"></div>
-    <img class="screenshotImage"  src="${imgURL}">
-    <div class="screenshotContent fadeIn-bottom">
-        <img class="screenshotOverlayImage" src="img/svgicons/002-website.png">
-        <p>View More</p>
-    </div>
-</div>`).join('');
-
-return `
-<div class="portfolio-piece portfolio-${x.portfolio_id}">
-    <div class="portfolio-piece-content">
-        <div class="portfolio-content">
-            <h2>
-                ${x.portfolio_title}
-            </h2>
-            <p>
-                ${x.portfolio_copy}
-            </p>
-        </div>
-        <div class="talentSection">
-            <h3>
-                Talent Showcased:
-            </h3>
-            <div>
-                ${talentListMapped}
-            </div>
-        </div>
-        <div class="linkSection">
-            <a href="${x.portfolio_url}" target="_blank" class="button urlBtn text-center">
-                Launch Site
-            </a>
-            <a href="${x.portfolio_githubUrl}" target="_blank" class="button githubBtn text-center">
-                View Code
-            </a>
-        </div>
-        <div class="portfolioImages">
-            ${screenShotImages}
-        </div>
-    </div>
-</div>
-` 
-}).join('');
-document.querySelector('#portfolioBios').innerHTML= porfolioBios;
-portP.map( function (x){
-    const portfolioNumber = x.portfolio_id;
-    const p = portfolioNumber.toString();
-    if(x.portfolio_url == false){
-        const linkBtns = document.getElementsByClassName("urlBtn")[p];
-        linkBtns.classList.add("nolink");
-     };
-     if(x.portfolio_githubUrl == false){
-        const linkBtns = document.getElementsByClassName("githubBtn")[p];
-        linkBtns.classList.add("nolink");
-     };
-});
+        </a>
+    </li>`).join('');
+    document.querySelector('#portPPP').innerHTML= porfolioCards;
 
 
 //////js for the stick nav on scroll
@@ -230,8 +37,10 @@ const observer = new window.IntersectionObserver(navHandler)
 // this is watching the hero banner
 observer.observe(hero)
 
- 
 
+
+
+// This is giving the hero elements motions. 
 window.addEventListener('scroll', function(e){
     const scrolled = window.pageYOffset;
     const fname = document.querySelector('.fristName');
@@ -254,94 +63,6 @@ window.addEventListener('scroll', function(e){
 
 
 
-//const form = document.getElementById('form');
-const formSub = document.getElementById('formSubmit');
-const name = document.getElementById('name_form');
-const email = document.getElementById('email_form');
-const message = document.getElementById('message_form');
-/*formSub.addEventListener('click', function (e){
-     e.preventDefault();
-     checkInputs();
- });/*/
-
-//Im thinking there should be a better way to write this. 
-document.querySelector('#name_form').addEventListener('keydown', clearError);
-document.querySelector('#email_form').addEventListener('keydown', clearError);
-
-
-function charCount(x){
-    const textLength = x.value.length;
-    document.querySelector('#numberOfChar').innerText=textLength;
-    if(textLength > 140){
-        setErrorMessage(message, 'Your message is to long.')
-    }else{
-        message.parentElement.classList.remove('error');
-    }
-};
-
-
-function checkInputs() {
-    event.preventDefault();
-    const nameValue = name.value.trim();
-    const emailValue = email.value.trim();
-    const messageValue = message.value.trim(); 
-
-if(nameValue === ""){
-    setErrorMessage(name, 'name can not be blank')
-    name.value="";
-};
-if(emailValue === ""){
-    setErrorMessage(email, 'email can not be blank')
-    email.value="";
-}else if (!isEmail(emailValue)) {
-    setErrorMessage(email, 'email must be valid')
-};
-if(messageValue === ""){
-    setErrorMessage(message, 'name can not be blank')
-    name.value="";
-};
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function setErrorMessage(input, message) {
-    const formOption = input.parentElement;
-    const small = formOption.querySelector('small');
-    formOption.className = 'formOption error';
-    small.innerText = message;
-}
-
-function clearError() {
-    //console.log('this is this');
-    //console.log(this.parentElement);
-    const partentHasErrorClass = this.parentElement.getAttribute('class');
-    if(partentHasErrorClass == 'formOption error'){
-     const errorField = document.querySelector('.formOption.error');
-     errorField.classList.remove('error');
-     console.log('this is the keydown functionm');
-    }
-     
- };
-
- function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
 
 
 
